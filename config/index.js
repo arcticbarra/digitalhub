9926903f23e3a86a7e729e9b4d9914e57b155c9f
Enrique Barragan <ebarragan1997@gmail.com>
2017-01-11 15:01:10 -0600

    Replaced filler text

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   getDbConnectionString: function() {
-    return process.env.MONGODB_URI;
+    return "mongodb://heroku_1tnw3tj1:k1ampmg3tb6vkr0ag3enak002p@ds161008.mlab.com:61008/heroku_1tnw3tj1"; //process.env.MONGODB_URI;
   }
 };
